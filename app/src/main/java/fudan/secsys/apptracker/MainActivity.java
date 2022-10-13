21dba8c6f64554894aac1a7cb79b9643b5886171
liuzc0506 <1902359074@qq.com>
2022-10-13 20:30:04 +0800

    interact with database

--- app/src/main/java/fudan/secsys/apptracker/MainActivity.java
+++ app/src/main/java/fudan/secsys/apptracker/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
