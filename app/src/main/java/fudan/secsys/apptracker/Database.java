d363e3b7bcab438d1b51638b252fe8718dceb39f
liuzc0506 <1902359074@qq.com>
2022-10-21 15:59:48 +0800

    Tables with pkg and time stamp

--- app/src/main/java/fudan/secsys/apptracker/Database.java
+++ app/src/main/java/fudan/secsys/apptracker/Database.java
@@ -1,63 +1,40 @@
 package fudan.secsys.apptracker;
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.util.Log;
+import android.content.Context;
 
 import java.sql.*;
+import java.util.List;
 
 public class Database {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     //设备和数据库须在同一网络下
-    static final String DB_URL = "jdbc:mysql://192.168.251.179:3306/log";
-    // static final String DB_URL = "jdbc:mysql://DESKTOP-8BHQG8P/log";
+    static final String DB_URL = "jdbc:mysql://10.177.83.229:3306/log";
+    //static final String DB_URL = "jdbc:mysql://192.168.251.179:3306/log";
+
 
     static final String USER = "root";
     static final String PASS = "zzz123456.";
 
-    public static void insert(String serviceName, String methodName, String parameters, int callingUid, int callingPid) throws Exception {
+    public static void createTab(String tabName){
         Connection conn = null;
         PreparedStatement stmt = null;
+
         try {
             // 注册 JDBC 驱动器
             Class.forName(JDBC_DRIVER);
             //Log.d("InteractingDatabase","START INSERT!!!");
 
             // 打开连接
-            //System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             //Log.d("InteractingDatabase","CONNECTED WITH DATABASE!!!");
 
-            // 执行sql语句
-            //System.out.println("Creating statement...");
-            String sql = "INSERT into loginfo value(?,?,?,?,?);";
-            stmt = conn.prepareStatement(sql);
+            // 创建数据表
+            String createTabSql = "CREATE TABLE " + tabName + "(timeStamp varchar(30), serviceName varchar(50), methodName varchar(50), parameters varchar(1024), callingUid int,callingPid int);";
 
-            // 参数赋值，参数从左至右序号为 1，2...
-            stmt.setString(1, serviceName);
-            stmt.setString(2, methodName);
-            stmt.setString(3, parameters);
-            stmt.setInt(4, callingUid);
-            stmt.setInt(5, callingPid);
-
-            //执行sql语句，返回类型为int，代表操作的数据条数
-            int rows = stmt.executeUpdate();// int executeUpdate(String SQL)
-            //System.out.println("被影响的行数 : " + rows);
-            //Log.d("InteractingDatabase","FINISH INSERT!!!");
-
-            /*
-             * sql = "SELECT id, name, age FROM info";
-             * ResultSet rs = stmt.executeQuery(sql);// 执行查询操作时使用此方法可以生成一个Resultset集合
-             *
-             * while (rs.next()) {
-             * int id = rs.getInt("id");
-             * int age = rs.getInt("age");
-             * String name = rs.getString("name");
-             *
-             * System.out.print("ID: " + id);
-             * System.out.print(", Age: " + age);
-             * System.out.print(", Name: " + name);
-             * System.out.println();
-             * }
-             * rs.close();
-             */
+            stmt = conn.prepareStatement(createTabSql);
+            stmt.executeUpdate();
 
             stmt.close();
             conn.close();
@@ -78,6 +55,93 @@ public class Database {
                 se.printStackTrace();
             }
         }
-        //System.out.println("Goodbye!");
+    }
+
+    public static void insert(String tabName, String timeStamp, String serviceName, String methodName, String parameters, int callingUid, int callingPid) throws Exception {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            // 注册 JDBC 驱动器
+            Class.forName(JDBC_DRIVER);
+            //Log.d("InteractingDatabase","START INSERT!!!");
+
+            // 打开连接
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            //Log.d("InteractingDatabase","CONNECTED WITH DATABASE!!!");
+
+            // 执行sql语句
+            String sql = "INSERT into " + tabName + " value(?,?,?,?,?,?);";
+            stmt = conn.prepareStatement(sql);
+
+            // 参数赋值，参数从左至右序号为 1，2...
+            stmt.setString(1, timeStamp);
+            stmt.setString(2, serviceName);
+            stmt.setString(3, methodName);
+            stmt.setString(4, parameters);
+            stmt.setInt(5, callingUid);
+            stmt.setInt(6, callingPid);
+
+            //执行sql语句，返回类型为int，代表操作的数据条数
+            stmt.executeUpdate();// int executeUpdate(String SQL)
+            //Log.d("InteractingDatabase","FINISH INSERT!!!");
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {// 关闭资源
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+    public static void dropTab(String tabName){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            // 注册 JDBC 驱动器
+            Class.forName(JDBC_DRIVER);
+            //Log.d("InteractingDatabase","START INSERT!!!");
+
+            // 打开连接
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            //Log.d("InteractingDatabase","CONNECTED WITH DATABASE!!!");
+
+            // 删除数据表
+            String dropTabSql = "DROP table " + tabName + ";";
+            stmt = conn.prepareStatement(dropTabSql);
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {// 关闭资源
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/fudan/secsys/apptracker/Tracker.java
+++ app/src/main/java/fudan/secsys/apptracker/Tracker.java
@@ -1,46 +1,95 @@
 package fudan.secsys.apptracker;
 
+import android.accessibilityservice.AccessibilityService;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.location.Location;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 import fudan.secsys.apptracker.Database;
 
 public class Tracker {
     static class LogThread extends Thread {
         int flag = 1;
+        static private String pkgName;
+
+        public LogThread(String app_name) {
+            pkgName = app_name;
+        }
+
         public void run() {
             try {
                 Process process = Runtime.getRuntime().exec("logcat");
                 InputStream is = process.getInputStream();
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
                 String line;
+                //创建数据表
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+                Date date = new Date(System.currentTimeMillis());
+                String time = simpleDateFormat.format(date);
+                String tabname= "`"+pkgName+" "+time+"`";
+                Database.createTab(tabname);
+                boolean isEmptyTab = true;
+
                 while ((line = br.readLine()) != null && flag==1) {
-                    //if (line.contains("AppTracker") && (!line.contains("MaldectTest"))) {
-                    if(line.contains("Maldetect")){
-                        //Todo: SQLite
+                    if(line.contains("Maldetect")) {
                         int index;
                         int callingUid = Integer.parseInt(line.substring(line.indexOf("callingUid:") + "callingUid:".length(),
                                 index = line.indexOf(",")));
                         int callingPid = Integer.parseInt(line.substring(line.indexOf("callingPid:") + "callingPid:".length(),
                                 index = line.indexOf(",", index + 1)));
-                        String serviceName = line.substring(index + 1,
-                                index = line.indexOf(".", index + 1));
-                        String methodName = line.substring(index + 1,
-                                index = line.indexOf("(", index + 1));
-                        String parameters = line.substring(index + 1,
-                                index = line.indexOf(")", index + 1));
-                        if(parameters.length() == 0)
-                            parameters=null;
 
-                        Database.insert(serviceName, methodName, parameters, callingUid, callingPid);
+                        //获取包名
+                        String callingPkgName = "";
+                        ActivityManager activityManager = (ActivityManager) MyApplication.getContext().getSystemService(Context.ACTIVITY_SERVICE);
+                        if (activityManager != null) {
+                            List<ActivityManager.RunningAppProcessInfo> list = activityManager.getRunningAppProcesses();
+                            for (ActivityManager.RunningAppProcessInfo info : list) {
+                                if (info.pid == callingPid) {
+                                    callingPkgName = info.processName;
+                                    break;
+                                }
+                            }
+                        }
+                        //if (line.contains("AppTracker") && (!line.contains("MaldectTest"))) {
+                        if (callingPkgName.contains(pkgName)) {
+                            //Todo: SQLite
+                            int space = line.indexOf(" ");
+                            space = line.indexOf(" ", space + 1);
+                            String timeStamp = line.substring(0, space);
+                            System.out.println(timeStamp);
 
-                        Log.d("MaldectTest.Log", line);
+                            String serviceName = line.substring(index + 1,
+                                    index = line.indexOf(".", index + 1));
+                            String methodName = line.substring(index + 1,
+                                    index = line.indexOf("(", index + 1));
+                            String parameters = line.substring(index + 1,
+                                    line.indexOf(")", index + 1));
+                            if (parameters.length() == 0)
+                                parameters = null;
+
+                            Database.insert(tabname, timeStamp, serviceName, methodName, parameters, callingUid, callingPid);
+                            isEmptyTab = false;
+
+                            Log.d("MaldectTest.Log", line);
+                        }
                     }
+                }
+                //删除空表
+                if(isEmptyTab){
+                    Database.dropTab(tabname);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -70,7 +119,7 @@ public class Tracker {
     static LogThread t_log;
     static BPFThread t_bpf;
     static void startTrack(String app_name){
-        t_log=new LogThread();
+        t_log=new LogThread(app_name);
         t_log.flag=1;
         t_log.start();
         //t_bpf=new BPFThread();
